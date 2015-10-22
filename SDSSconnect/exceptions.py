fda5f05451609bb28684808c17075117c9461caf
j.sanchezgallego <j.sanchezgallego@pegasus.pa.uky.edu>
2015-10-22 14:00:05 +0000

    Merge branch 'GitHubBranch'

---
+++ SDSSconnect/exceptions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+exceptions.py
+
+Created by José Sánchez-Gallego on 15 Oct 2015.
+Licensed under a 3-clause BSD license.
+
+Revision history:
+    15 Oct 2015 J. Sánchez-Gallego
+      Initial version
+
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+class SDSSconnectError(Exception):
+    """Base exception for SDSSconnect. Other exceptions should inherit this."""
+    pass
+
+
+class SDSSconnectWarning(Warning):
+    """Base warning for SDSSconnect."""
+    pass
+
+
+class SDSSconnectUserWarning(UserWarning, SDSSconnectWarning):
+    """The primary warning class."""
+    pass
