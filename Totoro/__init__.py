d8c98876b7c1ccfcb5cccf8ac174978c51b5d728
José Sánchez-Gallego <gallegoj@uw.edu>
2018-06-07 13:17:46 -0700

    :art: Clean __init__

--- Totoro/__init__.py
+++ Totoro/__init__.py
@@ -1,23 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2018-06-07
+# @Filename: __init__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+# @Copyright: José Sánchez-Gallego
+
 from __future__ import absolute_import
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import warnings
+
 from .exceptions import DustMapWarning, TotoroError
+from .readPath import readPath
+
+
 # warnings.filterwarnings('ignore', module='astropy.time.core')
 warnings.filterwarnings('ignore', 'Module argparse was already imported')
 warnings.filterwarnings(
     'ignore', 'Skipped unsupported reflection of ' + 'expression-based index q3c_field_idx')
 
-from .readPath import readPath
 __DEFAULT_CONFIG_FILE__ = readPath('+defaults.yaml')
 __TOTORO_CONFIG_PATH__ = readPath('~/.totoro/totoro.yaml')
 
-# Reads the configuration file
-from .core.configuration import getConfiguration
+from .core.configuration import getConfiguration  # noqa
 config = getConfiguration()
 
 # Creates the custom logging system
-from .core.logger import initLog
+from .core.logger import initLog  # noqa
 log = initLog()
 log.debug('Logging starts now.')
 log.debug('Configuration file has been loaded.')
@@ -31,10 +40,7 @@ except (ImportError, ValueError):
 except Exception:
     raise TotoroError('something went wrong while importing the dust map.')
 
-from .utils.site import Site
+from .utils.site import Site  # noqa
 site = Site()
-
-from Totoro.dbclasses import *
-from Totoro.scheduler import Planner, Plugger
 
 __version__ = '2.0.0dev'
